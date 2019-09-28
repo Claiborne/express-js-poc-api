f77a6e295c3167ac4895e2d5ad6613b04562fbe0
Will Claiborne <Claiborne@users.noreply.github.com>
2019-09-28 15:52:50 -0700

    Fix indentation

--- index.js
+++ index.js
@@ -6,66 +6,65 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000
 app.listen(port, function() {
-	console.log(`Listening on port ${port}...`)
+  console.log(`Listening on port ${port}...`)
 })
 
 const animals = [
-	{ id: 1, name: 'lion'},
-	{ id: 2, name: 'leopard'},
-	{ id: 3, name: 'kangaroo'}
+  { id: 1, name: 'lion'},
+  { id: 2, name: 'leopard'},
+  { id: 3, name: 'kangaroo'}
 ]
 
 app.get('/', function(req, res) {
-	res.send('Welcome to my animals API')
+  res.send('Welcome to my animals API')
 })
 
 app.get('/api/animals', function(req, res) {
-	res.send(animals);
+  res.send(animals);
 })
 
 app.get('/api/animals/:id', function(req, res) {
-	const animal = animals.find(a => a.id === parseInt(req.params.id))
-	if (!animal) return res.status(404).send('Animal not found')
-	res.send(animal)
+  const animal = animals.find(a => a.id === parseInt(req.params.id))
+  if (!animal) return res.status(404).send('Animal not found')
+  res.send(animal)
 })
 
 app.post('/api/animals', function(req, res) {
-	const { error } = validateAnimal(req.body)
-	if (error) return res.status(400).send(error.details)
+  const { error } = validateAnimal(req.body)
+  if (error) return res.status(400).send(error.details)
 
-	const animal = {
-		id: animals.length + 1,
-		name: req.body.name
-	}
-	animals.push(animal)
-	res.send(animal)
+  const animal = {
+    id: animals.length + 1,
+    name: req.body.name
+  }
+  animals.push(animal)
+  res.send(animal)
 })
 
 app.put('/api/animals/:id', function(req, res) {
-	const animal = animals.find(a => a.id === parseInt(req.params.id))
-	if (!animal) return res.status(404).send('Animal not found')
+  const animal = animals.find(a => a.id === parseInt(req.params.id))
+  if (!animal) return res.status(404).send('Animal not found')
 
-	const { error } = validateAnimal(req.body)
-	if (error) return res.status(400).send(error.details)
+  const { error } = validateAnimal(req.body)
+  if (error) return res.status(400).send(error.details)
 
-
-	animal.name = req.body.name;
-	res.send(animal)
+  animal.name = req.body.name;
+  res.send(animal)
 })
 
 app.delete('/api/animals/:id', function(req, res) {
-	const animal = animals.find(a => a.id === parseInt(req.params.id))
-	if (!animal) return res.status(404).send('Animal not found')
+  const animal = animals.find(a => a.id === parseInt(req.params.id))
+  if (!animal) return res.status(404).send('Animal not found')
 
-	const index = animals.indexOf(animal)
-	animals.splice(index, 1)
+  const index = animals.indexOf(animal)
+  animals.splice(index, 1)
 
-	res.send(animal)
+  res.send(animal)
 })
 
 function validateAnimal(animal) {
-	const schema = {
-		name: Joi.string().min(1).required()
-	}
-	return Joi.validate(animal, schema)
+  const schema = {
+    name: Joi.string().min(1).required()
+  }
+  return Joi.validate(animal, schema)
 }
